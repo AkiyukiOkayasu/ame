74a3c4f994ead6224d71d16eebb59c59fea4c545
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2020-04-24 00:23:15 +0900

    Add: nxplib.h

---
+++ nxplib.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2020, Akiyuki Okayasu
+ */
+
+#ifndef _NXPLIB_NXPLIB_H_
+#define _NXPLIB_NXPLIB_H_
+
+#include <stdint.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* _NXPLIB_NXPLIB_H_ */
