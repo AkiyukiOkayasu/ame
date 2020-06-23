cd3d4fb362cc82f493635ff21d7acbfe3ecc1026
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2020-06-23 12:55:37 +0900

    addModulo2Pi

---
+++ dsp.h
@@ -0,0 +1,37 @@
+
+// Copyright 2020 Akiyuki Okayasu.
+//
+// Author: Akiyuki Okayasu (akiyuki.okayasu@gmail.com)
+//
+// -------------------------------------------------------
+//
+// DSP utility functions
+
+#ifndef _NXPLIB_DSP_H_
+#define _NXPLIB_DSP_H_
+
+#include "nxplib.h"
+#include "constant.h"
+
+namespace nxplib
+{
+    // addModulo2Pi
+    // value: value to add and modulo 2pi
+    // increment:
+    // return: 0~2pi
+    inline float addModulo2Pi(float value, const float increment)
+    {
+        value += increment;
+        if (value > twoPi<float>)
+        {
+            do
+            {
+                value -= twoPi<float>;
+            } while (value > twoPi<float>);
+        }
+        return value;
+    }
+
+} // namespace nxplib
+
+#endif //_NXPLIB_DSP_H_
