b9990a6109f793f695eae99bd3721b1e2f7c0527
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2020-06-23 13:41:16 +0900

    AudioBufferの説明追加

--- buffer.h
+++ buffer.h
@@ -1,5 +1,5 @@
 // Copyright 2020 Akiyuki Okayasu.
-// 
+//
 // Author: Akiyuki Okayasu (akiyuki.okayasu@gmail.com)
 
 #ifndef _NXPLIB_BUFFER_H_
@@ -9,10 +9,14 @@
 
 #include <array>
 
-namespace nxplib {
-// AudioBuffer<int16_t, 2, 512> 2ch, 512samples
-template<typename T, size_t channels, size_t samples>
-using AudioBuffer = std::array<std::array<T, samples>, channels>;
-}
+namespace nxplib
+{
+    // AudioBuffer
+    // 二次元配列版std::arrayの型エイリアス
+    // 使用例：AudioBuffer<int16_t, 2, 512> 2ch, 512samples
+    template <typename T, size_t channels, size_t samples>
+    using AudioBuffer = std::array<std::array<T, samples>, channels>;
+
+} // namespace nxplib
 
 #endif // _NXPLIB_BUFFER_H_
