b90a791541193bf1de60656f9a5a4e4fc2a58f2a
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2020-06-27 03:27:39 +0900

    DSP便利関数

--- audio/dspHelpers.h
+++ audio/dspHelpers.h
@@ -4,22 +4,21 @@
 //
 // -------------------------------------------------------
 //
-// DSP utility functions
+// DSP helper functions
 
-#ifndef _NXPLIB_DSP_H_
-#define _NXPLIB_DSP_H_
+#ifndef _NXPLIB_DSP_HELPERS_H_
+#define _NXPLIB_DSP_HELPERS_H_
 
-#include "constant.h"
 #include <limits>
+
+#include "../math/constants.h"
 
 namespace nxplib
 {
-    constexpr float fEpsilon = std::numeric_limits<float>::epsilon();
-
-    // addModulo2Pi
-    // value: value to add and modulo 2pi
-    // increment:
-    // return: 0~2pi
+    /**
+     * add value and modulo 2p
+     * return: 0.0f ~ 2pi
+     */
     constexpr float addModulo2Pi(float value, const float increment) noexcept
     {
         value += increment;
@@ -29,27 +28,36 @@ namespace nxplib
         }
         return value;
     }
-    static_assert((addModulo2Pi(twoPi<float>, pi<float>) - pi<float>) <= fEpsilon,
+    static_assert((addModulo2Pi(twoPi<float>, pi<float>) - pi<float>) <= std::numeric_limits<float>::epsilon(),
                   "error: addModulo2Pi()");
 
-    // freqToPeriod
-    // 周波数→周期
+} // namespace nxplib
+
+/**
+ * Freq  / Period
+ * 周波数 /  周期
+ */
+namespace nxplib
+{
+    /** freqToPeriod
+     * 周波数→周期
+     */
     constexpr float freqToPeriod(const float freq) noexcept
     {
         return 1.0f / freq;
     }
-    static_assert((freqToPeriod(1000.0f) - 0.001f) <= fEpsilon,
+    static_assert((freqToPeriod(1000.0f) - 0.001f) <= std::numeric_limits<float>::epsilon(),
                   "error: freqToPeriod()");
 
-    // periodToFreq
-    // 周期→周波数
+    /** periodToFreq
+     * 周期→周波数
+     */
     constexpr float periodToFreq(const float period) noexcept
     {
         return 1.0f / period;
     }
-    static_assert((periodToFreq(5.0f) - 0.2f) <= fEpsilon,
+    static_assert((periodToFreq(5.0f) - 0.2f) <= std::numeric_limits<float>::epsilon(),
                   "error: periodToFreq()");
-
 } // namespace nxplib
 
-#endif //_NXPLIB_DSP_H_
+#endif // _NXPLIB_DSP_HELPERS_H_
