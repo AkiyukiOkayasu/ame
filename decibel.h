ff782c212d7a981ac2aacc1455b92ce15f013488
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2020-06-27 02:44:06 +0900

    Update: デシベル振幅変換をクラスの静的メンバ関数にした
    デシベル振幅変換で使うデフォルト値をprivateにして隠蔽した

--- decibel.h
+++ decibel.h
@@ -9,33 +9,41 @@
 #ifndef _NXPLIB_DECIBEL_H_
 #define _NXPLIB_DECIBEL_H_
 
+#include "nxplib.h"
 #include <cmath>
 #include <algorithm>
 
 namespace nxplib
 {
-    constexpr float defaultMinusInfinitydB = -100.0f;
-
-    // decibelsToGain
-    // dbtoa
-    // caliculate amplitude(gain) from dB
-    // return
-    inline float decibelsToGain(const float dB,
-                                const float minusInfinityDb = defaultMinusInfinitydB) noexcept
+    class Decibels
     {
-        return dB > minusInfinityDb ? std::pow(10.0f, dB * 0.05f)
-                                    : 0.0f;
-    }
+    public:
+        /** Caliculate amplitude(gain) from dB
+         * Max/MSPのdbtoaオブジェクト
+         */
+        static inline float decibelsToGain(const float dB,
+                                           const float minusInfinityDb = defaultMinusInfinitydB) noexcept
+        {
+            return dB > minusInfinityDb ? std::pow(10.0f, dB * 0.05f)
+                                        : 0.0f;
+        }
 
-    // gainToDecibels
-    // atodb
-    // calculate dB from amplitude(gain)
-    inline float gainToDecibels(const float gain,
-                                const float minusInfinityDb = defaultMinusInfinitydB) noexcept
-    {
-        return gain > 0.0f ? std::max(minusInfinityDb, std::log10(gain) * 20.0f)
-                           : minusInfinityDb;
-    }
+        /** Calculate dB from amplitude(gain)
+        * Max/MSPのatodbオブジェクト
+        */
+        static inline float gainToDecibels(const float gain,
+                                           const float minusInfinityDb = defaultMinusInfinitydB) noexcept
+        {
+            return gain > 0.0f ? std::max(minusInfinityDb, std::log10(gain) * 20.0f)
+                               : minusInfinityDb;
+        }
+
+    private:
+        static constexpr float defaultMinusInfinitydB = -100.0f; //-100dB以下は振幅ゼロにする
+
+        Decibels() = delete; // This class can't be instantiated, it's just a holder for static methods..
+        DISALLOW_COPY_AND_ASSIGN(Decibels);
+    };
 
 } // namespace nxplib
 
