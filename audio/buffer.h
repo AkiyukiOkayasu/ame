6b6f1f9e30bb7bac8ede48867c9e36d44f79847b
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2020-07-26 04:16:33 +0900

    Add: AudioMonoBuffer

--- audio/buffer.h
+++ audio/buffer.h
@@ -18,6 +18,11 @@ namespace ame
     template <typename T, size_t channels, size_t samples>
     using AudioBuffer = std::array<std::array<T, samples>, channels>;
 
+    // AudioMonoBuffer
+    // std::arrayの型エイリアス
+    // 使用例：AudioBuffer<int16_t, 512> 512samples
+    template <typename T, size_t samples>
+    using AudioMonoBuffer = AudioBuffer<T, samples, 1>;
 } // namespace ame
 
 #endif // _AME_BUFFER_H_
