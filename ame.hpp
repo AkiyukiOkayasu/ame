4df2559cd4fbc0b7e6f95a0bce19972ef44e8289
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2020-10-27 15:17:47 +0900

    Add: ame.hpp

---
+++ ame.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2020 Akiyuki Okayasu
+// Author: Akiyuki Okayasu (akiyuki.okayasu@gmail.com)
+// AME is released under the MIT license.
+// -------------------------------------------------------
+// ame.hpp
+
+#pragma once
+
+#include "audio/buffer.hpp"
+#include "audio/decibel.hpp"
+#include "audio/dspHelpers.hpp"
+#include "audio/q23_float_conversion.hpp"
+#include "audio/sine_oscillator.hpp"
+#include "math/constants.hpp"
+#include "midi/midiHelpers.hpp"
