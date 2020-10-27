2dfc4dd80838180f462ea2a6f318a4848a036df2
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2020-10-27 15:16:03 +0900

    数学定数の変数テンプレートをやめ、floatのみとした

---
+++ math/constants.hpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2020 Akiyuki Okayasu
+// Author: Akiyuki Okayasu (akiyuki.okayasu@gmail.com)
+// AME is released under the MIT license.
+// -------------------------------------------------------
+// Math constants
+
+#pragma once
+
+namespace ame
+{
+    static constexpr float pi = 3.14159265f;
+    static constexpr float twoPi = 6.283185307f;
+    static constexpr float halfPi = 1.57079632f;
+    static constexpr float euler = 2.71828182f;
+    static constexpr float sqrt2 = 1.41421356f;
+} // namespace ame
