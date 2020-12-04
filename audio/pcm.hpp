fb4feb78c41160df5339728c901783d2ab978150
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2020-12-04 13:20:55 +0900

    Add: pcm.hpp(まだ空)

---
+++ audio/pcm.hpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2020 Akiyuki Okayasu
+// Author: Akiyuki Okayasu (akiyuki.okayasu@gmail.com)
+// AME is released under the MIT license.
+// -------------------------------------------------------
+// PCM
+// WAV reader / writer
+
+#pragma once
+
+#include
