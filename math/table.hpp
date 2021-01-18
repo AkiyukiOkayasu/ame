7d985567eb4bb65f83e0ff8ca56231f18694c90f
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2021-01-18 22:31:53 +0900

    Add comment

--- math/table.hpp
+++ math/table.hpp
@@ -32,6 +32,22 @@ namespace ame
         return a;
     }
 
-    // TODO hamming窓とかの実装追加
+    // TODO hamming窓や矩形波などの実装追加
 
 } // namespace ame
+
+/*
+より汎用的な実装
+https://www.it-swarm-ja.tech/ja/c++/%E6%9C%80%E6%96%B0%E3%81%AEc-%EF%BC%9Aconstexpr%E3%83%86%E3%83%BC%E3%83%96%E3%83%AB%E3%81%AE%E5%88%9D%E6%9C%9F%E5%8C%96/835602258/
+
+template<class T, size_t N, class F>
+constexpr auto make_table(F func, T first)
+{
+    std::array<T, N> a {first};
+    for (size_t i = 1; i < N; ++i)
+    {
+        a[i] = func(a[i - 1]);
+    }
+    return a;
+}
+*/
