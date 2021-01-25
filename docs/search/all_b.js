64b83f539ac4ef1d76fbb5c5cbfe30df858f5d75
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2021-01-26 00:40:39 +0900

    doxygen

--- docs/search/all_b.js
+++ docs/search/all_b.js
@@ -7,7 +7,7 @@ var searchData=
   ['setslidedown_32',['setSlideDown',['../classame_1_1_slide.html#abc2dbee329c7f26ff75d1eb0e4fbb7a7',1,'ame::Slide']]],
   ['setslideup_33',['setSlideUp',['../classame_1_1_slide.html#ad3bba919f1b1123704542652e2e76ebd',1,'ame::Slide']]],
   ['sine_5foscillator_2ehpp_34',['sine_oscillator.hpp',['../sine__oscillator_8hpp.html',1,'']]],
-  ['sineoscillator_35',['SineOscillator',['../classame_1_1_sine_oscillator.html',1,'ame']]],
+  ['sineoscillator_35',['SineOscillator',['../classame_1_1_sine_oscillator.html',1,'ame::SineOscillator'],['../classame_1_1_sine_oscillator.html#ae3e9f889ed1c34ba7d45134071cfb9e3',1,'ame::SineOscillator::SineOscillator()']]],
   ['sinf_36',['sinf',['../special_functions_8hpp.html#a04a55b89aafb7536f4f731002eb232ea',1,'ame']]],
   ['slide_37',['Slide',['../classame_1_1_slide.html',1,'ame::Slide'],['../classame_1_1_slide.html#ac538d8f438b0638f45146a9d7a581f51',1,'ame::Slide::Slide()']]],
   ['slide_2ehpp_38',['slide.hpp',['../slide_8hpp.html',1,'']]],
--- docs/search/functions_a.js
+++ docs/search/functions_a.js
@@ -5,6 +5,7 @@ var searchData=
   ['setfrequency_77',['setFrequency',['../classame_1_1_sine_oscillator.html#aa733e87a8bb4d568767fda009a074162',1,'ame::SineOscillator']]],
   ['setslidedown_78',['setSlideDown',['../classame_1_1_slide.html#abc2dbee329c7f26ff75d1eb0e4fbb7a7',1,'ame::Slide']]],
   ['setslideup_79',['setSlideUp',['../classame_1_1_slide.html#ad3bba919f1b1123704542652e2e76ebd',1,'ame::Slide']]],
-  ['sinf_80',['sinf',['../special_functions_8hpp.html#a04a55b89aafb7536f4f731002eb232ea',1,'ame']]],
-  ['slide_81',['Slide',['../classame_1_1_slide.html#ac538d8f438b0638f45146a9d7a581f51',1,'ame::Slide']]]
+  ['sineoscillator_80',['SineOscillator',['../classame_1_1_sine_oscillator.html#ae3e9f889ed1c34ba7d45134071cfb9e3',1,'ame::SineOscillator']]],
+  ['sinf_81',['sinf',['../special_functions_8hpp.html#a04a55b89aafb7536f4f731002eb232ea',1,'ame']]],
+  ['slide_82',['Slide',['../classame_1_1_slide.html#ac538d8f438b0638f45146a9d7a581f51',1,'ame::Slide']]]
 ];
