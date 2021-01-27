70b21efc49a05e92dbced230aa24eda4039dc54b
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2021-01-28 01:50:39 +0900

    Add: 補間に関するheader

---
+++ audio/interpolation.hpp
@@ -0,0 +1,23 @@
+/** 
+    @file interpolation.hpp
+    @author Akiyuki Okayasu (akiyuki.okayasu@gmail.com)
+    @copyright Copyright (c) 2021 - Akiyuki Okayasu
+    
+    AME is released under the MIT license.
+*/
+
+#pragma once
+
+#include <atomic>
+
+namespace ame
+{
+/** Linear interpolation between a and b.
+    @note Function as same as std::lerp() implemented in C++20.
+    @param a Start point
+    @param b Target point
+    @param t It expects a value of 0~1 to be entered, but specifications exceeding 1 are allowed.
+    @return linear interpolated value
+*/
+constexpr float lerp (float a, float b, float t) noexcept { return a + t * (b - a); }
+} // namespace ame
