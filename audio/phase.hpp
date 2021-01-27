68ccead73dd161bc395c10cd1edd88c2be4159b4
akiyuki@tkrworks.net <akiyuki@tkrworks.net>
2021-01-28 01:52:37 +0900

    Add: phase.hpp

---
+++ audio/phase.hpp
@@ -0,0 +1,28 @@
+/** 
+    Some utilities about phase operation.    
+    @file phase.hpp
+    @author Akiyuki Okayasu (akiyuki.okayasu@gmail.com)
+    @copyright Copyright (c) 2021 - Akiyuki Okayasu
+
+    AME is released under the MIT license.
+*/
+
+#include "../math/constants.hpp"
+
+namespace ame
+{
+/** Increment the phase and returns in the range of 0~2pi.
+    @param phase
+    @param increment Amount to add to phase
+    @return [0, 2pi]
+*/
+constexpr float addModulo2Pi (float phase, const float increment) noexcept
+{
+    phase += increment;
+    while (phase > twoPi)
+    {
+        phase -= twoPi;
+    }
+    return phase;
+}
+} // namespace ame
