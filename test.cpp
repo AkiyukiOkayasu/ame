e4651b2692c5baecfbc8fb54f3ccfd8c5203406b
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-02-25 21:43:45 +0900

    unit test

--- test.cpp
+++ test.cpp
@@ -4,45 +4,60 @@
 #include <catch2/catch.hpp>
 #include <iomanip>
 
-TEST_CASE ("Decibels")
+TEST_CASE ("Volume")
 {
     SECTION ("decibelsToGain()")
     {
-        REQUIRE (ame::dBToGain (0.0f) == Approx (1.0f));
-        REQUIRE (ame::dBToGain (-6.0f) == Approx (0.501187));
-        REQUIRE (ame::dBToGain (-20.0f) == Approx (0.1));
-        REQUIRE (ame::dBToGain (3.0f) == Approx (1.412538));
+        REQUIRE (ame::decibelsToGain (0.0f) == Approx (1.0f));
+        REQUIRE (ame::decibelsToGain (-6.0f) == Approx (0.501187));
+        REQUIRE (ame::decibelsToGain (-20.0f) == Approx (0.1));
+        REQUIRE (ame::decibelsToGain (3.0f) == Approx (1.412538));
     }
     SECTION ("gainToDecibels()")
     {
-        REQUIRE (ame::gainTodB (1.0f) == Approx (0.0f));
-        REQUIRE (ame::gainTodB (0.501187f) == Approx (-6.0f));
-        REQUIRE (ame::gainTodB (0.1f) == Approx (-20.0f));
-        REQUIRE (ame::gainTodB (1.412538f) == Approx (3.0f));
+        REQUIRE (ame::gainToDecibels (1.0f) == Approx (0.0f));
+        REQUIRE (ame::gainToDecibels (0.501187f) == Approx (-6.0f));
+        REQUIRE (ame::gainToDecibels (0.1f) == Approx (-20.0f));
+        REQUIRE (ame::gainToDecibels (1.412538f) == Approx (3.0f));
     }
 }
 
-TEST_CASE ("dspHelpers")
+TEST_CASE ("Time")
 {
     SECTION ("addModule2Pi()")
     {
         REQUIRE (ame::addModulo2Pi (6.28318530717958647692f, 0.1f) == Approx (0.1f));
         REQUIRE (ame::addModulo2Pi (6.28318530717958647692f, 2.5f) == Approx (2.5f));
     }
+
     SECTION ("freqToPeriod()")
     {
         REQUIRE (ame::freqToPeriod (440.0f) == Approx (0.002272727f));
         REQUIRE (ame::freqToPeriod (44100.0f) == Approx (0.000022676f));
     }
+
     SECTION ("periodToFreq()")
     {
         REQUIRE (ame::periodToFreq (0.002272727f) == Approx (440.0f));
         REQUIRE (ame::periodToFreq (0.000022676f) == Approx (44100.0f));
     }
+
     SECTION ("scale()")
     {
         REQUIRE (ame::scale (0.5f, 0.0f, 1.0f, -100.0f, 100.0f) == Approx (0.0f));
         REQUIRE (ame::scale (0.0f, 0.0f, 1.0f, -100.0f, 100.0f) == Approx (-100.0f));
+    }
+
+    SECTION ("BPM to ms")
+    {
+        REQUIRE (ame::bpmToMs (60.0f) == Approx (1000.0f));
+        REQUIRE (ame::bpmToMs (120.0f) == Approx (500.0f));
+    }
+
+    SECTION ("ms to BPM")
+    {
+        REQUIRE (ame::msToBpm (1000.0f) == Approx (60.0f));
+        REQUIRE (ame::msToBpm (500.0f) == Approx (120.0f));
     }
 }
 
@@ -58,10 +73,6 @@ TEST_CASE ("WaveTable")
     {
         auto f = [] (auto& x) { x = ame::sinf (x * ame::twoPi); };
         auto ar = ame::make_waveTable<float, 5> (f);
-        for (auto&& e : ar)
-        {
-            std::cout << std::fixed << std::setprecision (5) << e << std::endl;
-        }
 
         /* 
         -0.0f == Approx(0.0f)を失敗させないために、Approx().scale(1)を指定する
