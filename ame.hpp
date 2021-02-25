2b952a74709f7174206053610740ff01a563748e
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-02-25 21:43:31 +0900

    ame.hppのincludeを整理した

--- ame.hpp
+++ ame.hpp
@@ -1,4 +1,5 @@
 /** 
+    main header file.
     @file ame.hpp
     @author Akiyuki Okayasu (akiyuki.okayasu@gmail.com)
     @copyright Copyright (c) 2021 - Akiyuki Okayasu
@@ -8,18 +9,17 @@
 
 #pragma once
 
-// TODO include整理と確認
-
 #include "audio/buffer.hpp"
 #include "audio/frequency.hpp"
 #include "audio/interpolation.hpp"
 #include "audio/mapping.hpp"
 #include "audio/phase.hpp"
 #include "audio/q23_float_conversion.hpp"
+#include "audio/sine_oscillator.hpp"
 #include "audio/smoothing.hpp"
 #include "audio/time.hpp"
 #include "audio/volume.hpp"
+#include "audio/wavetable.hpp"
 #include "math/constants.hpp"
 #include "math/specialFunctions.hpp"
-#include "math/table.hpp"
 #include "system/bytes.hpp"
