7a8317493a70be5293111da0befd459771284ef2
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-02-25 21:42:41 +0900

    dB系関数をリネーム

--- audio/volume.hpp
+++ audio/volume.hpp
@@ -13,24 +13,32 @@
 
 namespace ame
 {
-/** Convert dB to amplitude.
+/** Convert a dBFS value to to its equivalent gain level.
     @param dB decibels value
     @return amplitude [0, 1]
     @note Outputs 0 if the input is less than -100dB.
+    @code
+    decibelsToGain (0.0f); // 1.0f
+    decibelsToGain (-20.0f); // 0.1f
+    @endcode
 */
-constexpr float dBToGain (const float dB) noexcept
+constexpr float decibelsToGain (const float dB) noexcept
 {
     return dB > -100.0f
                ? std::pow (10.0f, dB * 0.05f)
                : 0.0f; // Outputs 0 if the input is less than -100dB
 }
 
-/** Convert amplitude to dB.
+/** Convert a gain level into a dBFS value.
     @param gain 
     @return dB decibels value
     @note Outputs Outputs -100dB if the input is less than 0.00001.
+    @code
+    gainToDecibels (1.0f); // 0.0f
+    gainToDecibels (0.1f); // -20.0f
+    @endcode
 */
-constexpr float gainTodB (const float gain) noexcept
+constexpr float gainToDecibels (const float gain) noexcept
 {
     return gain > 0.00001f
                ? std::log10 (gain) * 20.0f
