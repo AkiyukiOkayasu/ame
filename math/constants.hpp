dc9f1988200ca999fd73cdece4beac17157ed554
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-06-24 17:39:30 +0900

    comment

--- math/constants.hpp
+++ math/constants.hpp
@@ -1,5 +1,5 @@
 /** 
-    Math constants
+    Math constants.
     @file constants.hpp
     @author Akiyuki Okayasu (akiyuki.okayasu@gmail.com)
     @copyright Copyright (c) 2021 - Akiyuki Okayasu
@@ -11,9 +11,10 @@
 
 namespace ame
 {
-constexpr float pi = 3.14159265f;
-constexpr float twoPi = 6.283185307f;
-constexpr float halfPi = 1.57079632f;
-constexpr float euler = 2.71828182f;
-constexpr float sqrt2 = 1.41421356f;
+constexpr float pi = 3.14159265f;      ///< π
+constexpr float twoPi = 6.283185307f;  ///< 2π
+constexpr float halfPi = 1.57079632f;  ///< π/2
+constexpr float euler = 2.71828182f;   ///< Napier's constant
+constexpr float sqrt2 = 1.41421356f;   ///< sqrt(2)
+constexpr float sqrt1_2 = 0.70710678f; ///< sqrt(1/2)
 } // namespace ame
