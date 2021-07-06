56100d59ba00e604425bc265bae3c6825ae8c01e
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-07-06 15:47:54 +0900

    Refactoring

--- ame.hpp
+++ ame.hpp
@@ -8,12 +8,6 @@
 */
 
 /*******************************************************************************
- The block below describes the properties of this module, and is read by
- the Projucer to automatically generate project code that uses it.
- For details about the syntax and how to create or use a module, see the
- JUCE Module Format.md file.
- 
- 
  BEGIN_JUCE_MODULE_DECLARATION
  
  ID:                 ame
@@ -27,7 +21,6 @@
  dependencies:
  
  END_JUCE_MODULE_DECLARATION
- 
  *******************************************************************************/
 
 #pragma once
