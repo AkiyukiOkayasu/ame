cc2dd18fcd1caf098c36ac08a87f7e1e5564b154
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-07-06 11:29:58 +0900

    comment

--- system/wrap.hpp
+++ system/wrap.hpp
@@ -1,5 +1,5 @@
 /** 
-    
+    The number to automatically wrap in the range [0, length-1].    
     @file wrap.hpp
     @author Akiyuki Okayasu (akiyuki.okayasu@gmail.com)
     @copyright Copyright (c) 2021 - Akiyuki Okayasu
@@ -13,7 +13,7 @@
 
 namespace ame
 {
-/** @todo comment
+/** The number to automatically wrap in the range [0, length-1].
     @tparam length 
 */
 template <int_fast32_t length>
@@ -25,28 +25,26 @@ public:
     Wrap() = default;
     ~Wrap() = default;
 
-    void reset()
+    /** Sets the current value to an arbitrary number.
+        When n is not in the range of 0~length-1, the wrapped number is used as the current value.
+        @param n 
+    */
+    void set (int_fast32_t n)
     {
-        num = 0;
-    }
+        while (n >= length)
+        {
+            n -= length;
+        }
 
-    void set (const int_fast32_t n)
-    {
+        while (n < 0)
+        {
+            n += length;
+        }
+
         num = n;
-
-        while (num >= length)
-        {
-            num -= length;
-        }
-
-        while (num < 0)
-        {
-            num += length;
-        }
     }
 
-    /**
-    
+    /** Get the current value or the number of the current value plus an offset.
         @param offset 
         @return int_fast32_t [0, length-1]
     */
@@ -65,11 +63,10 @@ public:
         return n;
     }
 
-    /**
-    
-    @return int_fast32_t [0, length-1]
+    /** Prefix increment.
+        @return size_t [0, length-1]
     */
-    int_fast32_t operator++() //前置インクリメント
+    int_fast32_t operator++()
     {
         ++num;
         if (num >= length)
@@ -79,11 +76,10 @@ public:
         return num;
     }
 
-    /**
-    
-    @return int_fast32_t  [0, length-1]
+    /** Postfix increment.
+        @return int_fast32_t  [0, length-1]
     */
-    int_fast32_t operator++ (int) //後置インクリメント
+    int_fast32_t operator++ (int)
     {
         num++;
         if (num >= length)
@@ -93,15 +89,15 @@ public:
         return num;
     }
 
-    /**
-    
-    @param add 
-    @return int_fast32_t  [0, length-1]
+    /** += operator.
+        Add and return wrapped number.
+        @param add 
+        @return int_fast32_t  [0, length-1]
     */
     int_fast32_t operator+= (int_fast32_t add)
     {
         num += add;
-        if (num >= length)
+        while (num >= length)
         {
             num -= length;
         }
