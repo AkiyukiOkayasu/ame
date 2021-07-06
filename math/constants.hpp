1c38590f27daaf0720895f13e7890bddb42959d9
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-07-06 12:55:51 +0900

    Fix: 数学定数のinline指定

--- math/constants.hpp
+++ math/constants.hpp
@@ -11,10 +11,10 @@
 
 namespace ame
 {
-constexpr float pi = 3.14159265f;      ///< π
-constexpr float twoPi = 6.283185307f;  ///< 2π
-constexpr float halfPi = 1.57079632f;  ///< π/2
-constexpr float euler = 2.71828182f;   ///< Napier's constant
-constexpr float sqrt2 = 1.41421356f;   ///< sqrt(2)
-constexpr float sqrt1_2 = 0.70710678f; ///< sqrt(1/2)
+inline constexpr float pi = 3.14159265f;      ///< π
+inline constexpr float twoPi = 6.283185307f;  ///< 2π
+inline constexpr float halfPi = 1.57079632f;  ///< π/2
+inline constexpr float euler = 2.71828182f;   ///< Napier's constant
+inline constexpr float sqrt2 = 1.41421356f;   ///< sqrt(2)
+inline constexpr float sqrt1_2 = 0.70710678f; ///< sqrt(1/2)
 } // namespace ame
