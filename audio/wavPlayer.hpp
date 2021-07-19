791c859194de6a747faf5773c95fd1578a528bed
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-07-19 16:08:29 +0900

    WavPlayerの実装したい関数郡をとりあえず定義だけした

--- audio/wavPlayer.hpp
+++ audio/wavPlayer.hpp
@@ -8,7 +8,12 @@
 */
 #pragma once
 
+#include "audioBlockView.hpp"
 #include "wavReader.hpp"
+
+#include <cstddef>
+
+///@todo PCM, Float, ADPCM用のデコーダクラス作成
 
 namespace ame::wav
 {
@@ -22,7 +27,19 @@ public:
     }
     ~WavPlayer() = default;
 
+    void play();
+    void pause();
+    void reset();
+    void process();
+    bool isPlaying();
+    uint32_t getNextReadPosition();
+    void setNextReadPosition();
+    void setLoopPlay();
+    bool isLooping();
+
 private:
     const WavReader reader;
+    uint32_t readPosition = 0;
+    bool loop = false;
 };
 } // namespace ame::wav
