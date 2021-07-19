edd3edbe67883f9d2ae3ca999acb5d1b43a6fe4d
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-07-19 17:02:54 +0900

    wavのデコード用クラスの抽象クラス

--- audio/wavPlayer.hpp
+++ audio/wavPlayer.hpp
@@ -42,4 +42,22 @@ private:
     uint32_t readPosition = 0;
     bool loop = false;
 };
+
+namespace
+{
+    class WavDecoderBase
+    {
+    public:
+        virtual float getSample (uint32_t channel, uint32_t sample) = 0;
+    };
+
+    class PCMDecoder : public WavDecoderBase
+    {
+    public:
+        float getSample (uint32_t channel, uint32_t sample) override
+        {
+            return 0.0f;
+        }
+    };
+} // namespace
 } // namespace ame::wav
