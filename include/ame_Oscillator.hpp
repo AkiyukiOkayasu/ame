0074aac9eb5321ba188439d889ecc1803978e6c7
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-10-13 14:23:22 +0900

    refactor: Oscillator related functions has compiled to ame_Oscillator.hpp

--- include/ame_Oscillator.hpp
+++ include/ame_Oscillator.hpp
@@ -1,5 +1,5 @@
 /** 
-    @file sine_oscillator.hpp
+    @file ame_Oscillator.hpp
     @author Akiyuki Okayasu (akiyuki.okayasu@gmail.com)
     @copyright Copyright (c) 2021 - Akiyuki Okayasu
     
@@ -8,14 +8,46 @@
 
 #pragma once
 
-#include "../math/constants.hpp"
-#include "../math/specialFunctions.hpp"
+#include "ame_Math.hpp"
 #include "ame_Util.hpp"
 
 #include <atomic>
 
 namespace ame
 {
+/** Wavetable generator.
+    @tparam FloatType float or double
+    @tparam NumSamples array size
+    @tparam Function function to generate wavetable
+    @note Functionの入力は0~1範囲の配列です    
+    @see make_sineTable()
+*/
+template <typename FloatType, size_t NumSamples, class Function>
+constexpr auto makeWaveTable (Function func)
+{
+    std::array<FloatType, NumSamples> ar;
+    std::iota (ar.begin(), ar.end(), 0);
+    std::for_each (ar.begin(), ar.end(), [] (auto& x)
+                   { x = x / (NumSamples - 1); }); // ar: 0~1が順に並んだ配列
+    std::for_each (ar.begin(), ar.end(), func);
+    return ar;
+}
+
+/** Sine wave wavetable generator.
+    @tparam N array size
+    @return constexpr std::array<float, N> sine wave table
+    @todo 初期位相いじれるようにする？
+*/
+template <size_t N>
+constexpr auto makeSineTable()
+{
+    auto f = [] (auto& x)
+    {
+        x = ame::sinf (x * ame::twoPi);
+    };
+    return ame::makeWaveTable<float, N> (f);
+}
+
 /** Sine wave oscillator.
     Generates a sine between -1.0~1.0.
 */
