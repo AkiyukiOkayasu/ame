a01367b9728450373bef37998ea080849661dc64
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-10-13 14:18:22 +0900

    refactor: ame::lerp is moved to ame_Math.hpp

--- include/ame_Util.hpp
+++ include/ame_Util.hpp
@@ -16,18 +16,6 @@
 
 namespace ame
 {
-/** Linear interpolation.
-    @note Function as same as std::lerp() implemented in C++20.
-    @param a Start point
-    @param b Target point
-    @param t It expects a value of 0~1 to be entered, but specifications exceeding 1 are allowed.
-    @return linear interpolated value
-*/
-constexpr float lerp (float a, float b, float t) noexcept
-{
-    return a + t * (b - a);
-}
-
 /** Increment the phase and returns in the range of 0~2pi.
     @param phase
     @param increment Amount to add to phase
