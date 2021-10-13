27b81aa79159f28c51b40f425e0fd25c2dc3f55c
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-10-14 01:28:27 +0900

    feat: add MIDI constants

--- include/ame_Midi.hpp
+++ include/ame_Midi.hpp
@@ -39,3 +39,67 @@ struct ControlChange
     uint8_t channel;       ///< [0, 15]
 };
 } // namespace ame::midi
+
+//==============================================================================
+//Constants
+namespace ame::midi
+{
+/** USB-MIDI CIN (Code Index Number).
+    https://pcm1723.hateblo.jp/entry/20150109/1420819489
+*/
+namespace CodeIndexNumber
+{
+    inline constexpr uint8_t SYSCOMMON_2BYTES = 0x02; ///< データ長2Byte
+    inline constexpr uint8_t SYSCOMMON_3BYTES = 0x03;
+    inline constexpr uint8_t SYSEX_START_OR_CONTINUE = 0x04;
+    inline constexpr uint8_t SYSEX_END_1BYTE = 0x05;  ///< SysEx最終パケット データ長1Byte
+    inline constexpr uint8_t SYSEX_END_2BYTES = 0x06; ///< SysEx最終パケット データ長2Byte
+    inline constexpr uint8_t SYSEX_END_3BYTES = 0x07; ///< SysEx最終パケット データ長3Byte
+    inline constexpr uint8_t NOTE_OFF = 0x08;
+    inline constexpr uint8_t NOTE_ON = 0x09;
+    inline constexpr uint8_t POLYPHONIC_KEY_PRESSURE = 0x0A;
+    inline constexpr uint8_t CONTROL_CHANGE = 0x0B;
+    inline constexpr uint8_t PROGRAM_CHANGE = 0x0C;   ///< データ長2Byte
+    inline constexpr uint8_t CHANNEL_PRESSURE = 0x0D; ///< データ長2Byte
+    inline constexpr uint8_t PITCH_BEND = 0x0E;
+    inline constexpr uint8_t SYS_REALTIME = 0x0F; ///< データ長1Byte
+} // namespace CodeIndexNumber
+
+/** MIDI SysEx.
+    0xF0, 0x7D, 0x7F, ~~~~~ , 0xF7
+*/
+namespace SysEx
+{
+    inline constexpr uint8_t BEGIN = 0xF0; ///< SysEx開始
+    inline constexpr uint8_t END = 0xF7;   ///< SysEx終了
+
+    namespace ManufacturerID
+    {
+        inline constexpr uint8_t NON_COMMERCIAL = 0x7D; ///< Universal SysEx 非営利
+        inline constexpr uint8_t NON_REALTIME = 0x7E;   ///< Universal SysEx 非リアルタイム
+        inline constexpr uint8_t REALTIME = 0x7F;       ///< Universal SysEx リアルタイム
+    }                                                   // namespace ManufacturerID
+
+    namespace DeviceID
+    {
+        inline constexpr uint8_t ALL = 0x7F;  ///< 全端末
+        inline constexpr uint8_t JADE = 0x76; ///< jade
+    }                                         // namespace DeviceID
+} // namespace SysEx
+
+namespace ChannelMessage
+{
+    inline constexpr uint8_t NOTE_ON = 0x90;
+    inline constexpr uint8_t NOTE_OFF = 0x80;
+} // namespace ChannelMessage
+
+namespace SystemRealtime
+{
+    inline constexpr uint8_t TIMING_CLOCK = 0xF8;
+    inline constexpr uint8_t START = 0xFA;
+    inline constexpr uint8_t CONTINUE = 0xFB;
+    inline constexpr uint8_t STOP = 0xFC;
+    inline constexpr uint8_t ACTIVE_SENSING = 0xFE;
+    inline constexpr uint8_t SYSTEM_RESET = 0xFF;
+} // namespace SystemRealtime
+} // namespace ame::midi
