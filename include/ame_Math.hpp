5cdc5e0d891658f148983c3e3863bac01e9a8367
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2021-10-13 13:59:57 +0900

    Move all headers to the "include"

--- include/ame_Math.hpp
+++ include/ame_Math.hpp
@@ -29,6 +29,13 @@ extern "C"
 
 namespace ame
 {
+inline constexpr float pi = 3.14159265f;      ///< π
+inline constexpr float twoPi = 6.283185307f;  ///< 2π
+inline constexpr float halfPi = 1.57079632f;  ///< π/2
+inline constexpr float euler = 2.71828182f;   ///< Napier's constant
+inline constexpr float sqrt2 = 1.41421356f;   ///< sqrt(2)
+inline constexpr float sqrt1_2 = 0.70710678f; ///< sqrt(1/2)
+
 /** sinf.
 
     When CMSIS-DSP is available, use arm_sin_f32() of CMSIS-DSP; when not, use std::sinf()
@@ -59,5 +66,17 @@ inline float cosf (float x)
 #else
     return std::cos (x); //libstdc++だとstd:cosf()は未定義なので、std::cos()にしている
 #endif
+}
+
+/** Linear interpolation.
+    @note Function as same as std::lerp() implemented in C++20.
+    @param a Start point
+    @param b Target point
+    @param t It expects a value of 0~1 to be entered, but specifications exceeding 1 are allowed.
+    @return linear interpolated value
+*/
+constexpr float lerp (float a, float b, float t) noexcept
+{
+    return a + t * (b - a);
 }
 } // namespace ame
